9a968889e9551ed5a3cdd2f61c25b637084f64d5
Anasemos Kassahun <anasemosaka@gmail.com>
2022-11-19 22:42:03 -0500

    wip

--- app/screens/Camera.js
+++ app/screens/Camera.js
@@ -1,15 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button, Image, TouchableOpacity } from 'react-native';
 import { useEffect, useRef, useState } from 'react';
-import { Camera } from 'expo-camera';
+import { Camera, CameraType } from 'expo-camera';
 import { shareAsync } from 'expo-sharing';
 import * as MediaLibrary from 'expo-media-library';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+
+
 
 export default function App() {
     let cameraRef = useRef();
     const [hasCameraPermission, setHasCameraPermission] = useState();
     const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
     const [photo, setPhoto] = useState();
+    const [type, setType] = useState(CameraType.back);
 
     useEffect(() => {
         (async () => {
@@ -25,6 +31,8 @@ export default function App() {
     } else if (!hasCameraPermission) {
         return <Text>Permission for camera not granted. Please change this in settings.</Text>
     }
+
+
 
     let takePic = async () => {
         let options = {
@@ -59,12 +67,16 @@ export default function App() {
             </SafeAreaView>
         );
     }
+    function toggleCameraType() {
+        setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+    }
 
     return (
         <Camera style={styles.container} ref={cameraRef}>
             <View style={styles.buttonContainer}>
-                <Button title="Take Pic" onPress={takePic} />
+                <Ionicons name="aperture-outline" size={70} color="white" onPress={takePic} style={styles.icon} />
             </View>
+            <MaterialCommunityIcons name="camera-flip-outline" size={32} color="white" onPress={toggleCameraType} style={styles.flip} />
             <StatusBar style="auto" />
         </Camera>
     );
@@ -77,11 +89,26 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     buttonContainer: {
-        backgroundColor: '#fff',
-        alignSelf: 'flex-end'
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
     },
     preview: {
         alignSelf: 'stretch',
-        flex: 1
+        flex: 1,
+    },
+    icon: {
+        backgroundColor: 'black',
+        height: 100,
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    flip: {
+        left: 170,
+        bottom: 80
+
     }
 });
